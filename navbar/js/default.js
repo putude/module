feccb5ca52f46841bff8c6f346420fb51054c31f
putude <putude@gmail.com>
2023-07-21 07:31:43 +0800

    fix listener

--- navbar/js/default.js
+++ navbar/js/default.js
@@ -2,15 +2,15 @@ document.getElementById('navicon').addEventListener('click', (elm) => {
   document.querySelector('.navbar').classList.toggle('show')
 })
 
-document.querySelectorAll('.nav-dropdown-toggle').forEach(dropdown=>{
-  dropdown.addEventListener('mouseup', elm => {
+document.querySelectorAll('.nav-dropdown-toggle').forEach(dropdown => {
+  dropdown.addEventListener('click', elm => {
     elm.preventDefault()
     let navClass = elm.target.parentNode.classList
     navClass.contains('expand') ? navClass.remove('expand') : navClass.add('expand')
   })
 })
 
-document.addEventListener('mousedown', elm => {
+document.addEventListener('click', elm => {
   !elm.target.classList.contains('nav-dropdown-toggle') &&
     document.querySelectorAll('.nav-dropdown').forEach(elm => {
       elm.classList.contains('expand') ? elm.classList.remove('expand') : ''
